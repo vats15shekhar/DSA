12bdcd9cd94f3cdd8861c4fcd14ee8a0dce2c4f3
Vatsala <vats15shekhar@gmail.com>
2024-03-08 19:54:55 +0530

    Custom comparator in java

---
+++ src/CustomComparator.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CustomComparator {
+
+    public void customComparator(int[] A)
+    {
+        Integer[] numbers = new Integer[A.length];
+        for(int i = 0 ; i < A.length ; i++)
+        {
+            numbers[i] = A[i];
+        }
+
+        // Sort the array in ascending order
+        Arrays.sort(numbers, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                // Custom comparator to sort in ascending order
+                return o1 - o2;
+            }
+        });
+
+        System.out.println("Sorted array in ascending order: " + Arrays.toString(numbers));
+
+        // Sort the array in descending order
+        Arrays.sort(numbers, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                // Custom comparator to sort in descending order
+                return o2 - o1;
+            }
+        });
+
+        System.out.println("Sorted array in descending order: " + Arrays.toString(numbers));
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,9 @@ public class Main {
         i.insertionSort(A);
         System.out.println(Arrays.toString(A));
 
+        CustomComparator c = new CustomComparator();
+        c.customComparator(A);
+
     }
 
     static int[] A = {10,3,8,15,6,12,2,18,7,15,14};
