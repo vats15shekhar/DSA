0aa6995d77dbef5684125b33ae9c00590ee46970
Vatsala <vats15shekhar@gmail.com>
2024-03-09 19:59:47 +0530

    First positive missing number without extra space

--- src/ArraysAdvanced/FindFirstMissingPositiveInteger.java
+++ src/ArraysAdvanced/FindFirstMissingPositiveInteger.java
@@ -4,16 +4,14 @@ public class FindFirstMissingPositiveInteger {
 
     public int findFirstMissingInt(int[] A)
     {
-        int n = A.length;
+        // With extra space. A boolean array
+       /* int n = A.length;
         int[] boolArr = new int[n+1];
-
-
         for (int j : A) {
             if (j <= n && j > 0) {
                 boolArr[j] = 1;
             }
         }
-
         for(int k = 1 ; k < boolArr.length ; k++)
         {
             if(boolArr[k] != 1)
@@ -21,8 +19,42 @@ public class FindFirstMissingPositiveInteger {
                 return k;
             }
         }
+        return boolArr.length ;*/
 
-        return boolArr.length ;
+        // Without extra space
+        int n = A.length;
+        int filler = n + 1;
+        int ind = 0;
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(A[i] <= 0)
+            {
+                A[i] = filler;
+            }
+        }
+
+        for(int j = 0 ; j <n ; j++)
+        {
+            if(Math.abs(A[j]) <= n && Math.abs(A[j]) > 0)
+            {// We are first checking if the Absolute value of the element is less than n and greater than 0
+             // In that case we go to the index position A[j] - 1. then make that element negative. Negative element indicates presence of the element
+                ind = Math.abs(A[j]) - 1;
+                A[ind] = A[ind] * -1;
+            }
+        }
+
+        //Now iterate on the entire array to check a positive integer. A positive integer indicates the absence of the element in the array
+        for(int k = 0 ; k < n ; k++)
+        {
+            if(A[k] > 0)
+            {
+                return k + 1;
+            }
+        }
+
+        // If all the elements are negative in the array. which means that the first missing number is n+ 1
+        return n+1;
 
     }
 
